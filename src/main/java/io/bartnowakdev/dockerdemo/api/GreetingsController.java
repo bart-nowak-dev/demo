47b6f167ff1408664e4e21754a7ccd5be18cdebe
Bartlomiej Nowak <bart.nowak.dev@gmail.com>
2026-02-26 22:49:34 +0100

    Added HTTP API to demonstrate communication with application

---
+++ src/main/java/io/bartnowakdev/dockerdemo/api/GreetingsController.java
@@ -0,0 +1,16 @@
+package io.bartnowakdev.dockerdemo.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class GreetingsController {
+
+    @GetMapping("/greetings")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
